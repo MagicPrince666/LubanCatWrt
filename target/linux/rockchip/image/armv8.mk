9394e2e78c58e6fa7a1894e5f3c344e129d077da
hejiawencc <hjw0415@outlook.com>
2022-10-18 08:46:11 +0800

    rockchip 添加lubancat系列板卡，并添加rk356x内核patch

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -2,12 +2,72 @@
 #
 # Copyright (C) 2020 Tobias Maedel
 
+define Device/embedfire_doornet1
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := DoorNet1
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := doornet1-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3328 | pine64-bin | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152 kmod-rtl8821cu
+endef
+TARGET_DEVICES += embedfire_doornet1
+
+define Device/embedfire_doornet2
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := DoorNet2
+  SOC := rk3399
+  UBOOT_DEVICE_NAME := doornet2-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3399 | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8169 -urngd
+endef
+TARGET_DEVICES += embedfire_doornet2
+
+define Device/embedfire_lubancat1
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat1
+  SOC := rk3566
+  UBOOT_DEVICE_NAME := lubancat1-rk3566
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk356x | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8125
+endef
+TARGET_DEVICES += embedfire_lubancat1
+
+define Device/embedfire_lubancat1n
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat1N
+  SOC := rk3566
+  UBOOT_DEVICE_NAME := lubancat1n-rk3566
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk356x | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8169 -urngd kmod-ata-ahci
+endef
+TARGET_DEVICES += embedfire_lubancat1n
+
+define Device/embedfire_lubancat2
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat2
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := lubancat2-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk356x | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
+endef
+TARGET_DEVICES += embedfire_lubancat2
+
+define Device/embedfire_lubancat2n
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat2N
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := lubancat2n-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk356x | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci kmod-ata-ahci-platform kmod-ata-core
+endef
+TARGET_DEVICES += embedfire_lubancat2n
+
 define Device/friendlyarm_nanopi-r2s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R2S
   SOC := rk3328
   UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3328 | pine64-bin | gzip | append-metadata
   DEVICE_PACKAGES := kmod-usb-net-rtl8152
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
@@ -15,11 +75,10 @@ TARGET_DEVICES += friendlyarm_nanopi-r2s
 define Device/friendlyarm_nanopi-r4s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R4S
-  DEVICE_VARIANT := 4GB LPDDR4
   SOC := rk3399
   UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8169
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3399 | pine64-bin | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8169 -urngd
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r4s
 
@@ -29,15 +88,27 @@ define Device/pine64_rockpro64
   SOC := rk3399
   UBOOT_DEVICE_NAME := rockpro64-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := -urngd
 endef
 TARGET_DEVICES += pine64_rockpro64
 
-define Device/radxa_rock-pi-4a
+define Device/radxa_rock-3a
   DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := ROCK Pi 4A
+  DEVICE_MODEL := ROCK3 Model A
+  SOC := rk3568
+  SUPPORTED_DEVICES := radxa,rock3a
+  UBOOT_DEVICE_NAME := rock-3a-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk356x | pine64-img | gzip | append-metadata
+endef
+TARGET_DEVICES += radxa_rock-3a
+
+define Device/radxa_rock-pi-4
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := ROCK Pi 4
   SOC := rk3399
-  SUPPORTED_DEVICES := radxa,rockpi4a radxa,rockpi4
+  SUPPORTED_DEVICES := radxa,rockpi4
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := -urngd
 endef
-TARGET_DEVICES += radxa_rock-pi-4a
+TARGET_DEVICES += radxa_rock-pi-4
