f942701317ff182a5d4935604ff461ff87df289a
hejiawencc <hjw0415@outlook.com>
2022-11-07 10:22:02 +0800

    rockchip 修复kernel5.15构建错误，更新部分kernel 6.0 patch文件

--- mk-fire.sh
+++ mk-fire.sh
@@ -8,6 +8,7 @@ echo "请输入要运行脚本的序号:"
 echo "[0] Exit Menu"
 echo "[1] Update feeds/config/download"
 echo "[2] Save Defconfig"
+echo "[3] Package Image"
 echo "---------------------------------------------------------"
 read input
 
@@ -19,6 +20,9 @@ case $input in
         ;;
     2)
         SCRIPTS=Defconfig
+        ;;
+    3)
+        SCRIPTS=Package
         ;;
     *)
         echo 'input type number error, exit !'
@@ -59,3 +63,89 @@ then
 
     echo '**********************write successful *********************'
 fi
+
+if [ $SCRIPTS = Package ] 
+then
+    echo '**************     begining package image       ************'
+
+
+    DEVICE_DIR='bin/targets/rockchip/armv8/'
+
+    echo "---------------------------------------------------------"
+    echo "please enter your choise device number:"
+    echo "[0] Exit Menu"
+    echo "[1] DoorNet1"
+    echo "[2] DoorNet2"
+    echo "[3] LubanCat1"
+    echo "[4] LubanCat1N"
+    echo "[5] LubanCat2"
+    echo "[6] LubanCat2N"
+    echo "---------------------------------------------------------"
+    read input
+
+    case $input in
+        0)
+            exit;;
+        1)
+            DEVICE_NAME=DoorNet1
+            OUTPUT_DIR=EmbedFire_DoorNet1/
+            device_name=embedfire_doornet1
+            ;;
+        2)
+            DEVICE_NAME=DoorNet2
+            OUTPUT_DIR=EmbedFire_DoorNet2/
+            device_name=embedfire_doornet2
+            ;;
+        3)
+            DEVICE_NAME=LubanCat1
+            OUTPUT_DIR=EmbedFire_LubanCat1/
+            device_name=lubancat1
+            ;;
+        4)
+            DEVICE_NAME=LubanCat1N
+            OUTPUT_DIR=EmbedFire_LubanCat1N/
+            device_name=lubancat1n
+            ;;
+        5)
+            DEVICE_NAME=LubanCat2
+            OUTPUT_DIR=EmbedFire_LubanCat2/
+            device_name=lubancat2
+            ;;
+        6)
+            DEVICE_NAME=LubanCat2N
+            OUTPUT_DIR=EmbedFire_LubanCat2N/
+            device_name=lubancat2n
+            ;;
+        *)
+            echo 'input device number error, exit !'
+            exit;;
+    esac
+
+
+    echo '**********************config set info *********************'
+    echo 'DEVICE_NAME=' $DEVICE_NAME
+    echo 'DEVICE_DIR =' $DEVICE_DIR
+    echo 'OUTPUT_DIR =' $OUTPUT_DIR
+    echo 'Package file path : '${DEVICE_DIR}${OUTPUT_DIR}
+
+
+    dirdate=`date '+%Y_%m_%d'`
+    namedate=`date '+%Y%m%d'`
+
+    mkdir -p ${DEVICE_DIR}${OUTPUT_DIR}${dirdate}
+
+    cp ${DEVICE_DIR}openwrt-rockchip-armv8-${device_name}-ext4-sysupgrade.img.gz ${DEVICE_DIR}${OUTPUT_DIR}${dirdate}/openwrt-${device_name}-ext4-${namedate}.img.gz
+    cp ${DEVICE_DIR}openwrt-rockchip-armv8-${device_name}-squashfs-sysupgrade.img.gz ${DEVICE_DIR}${OUTPUT_DIR}${dirdate}/openwrt-${device_name}-squashfs-${namedate}.img.gz
+    cp ${DEVICE_DIR}openwrt-rockchip-armv8-${device_name}.manifest ${DEVICE_DIR}${OUTPUT_DIR}${dirdate}/openwrt-${device_name}.manifest
+
+    rm -f ${DEVICE_DIR}${OUTPUT_DIR}${dirdate}/sha256sum
+    cd ${DEVICE_DIR}${OUTPUT_DIR}
+    sha256sum ${dirdate}/* > ${dirdate}/sha256sum
+
+
+
+
+
+    echo '**************** package image successful  *****************'
+fi
+
