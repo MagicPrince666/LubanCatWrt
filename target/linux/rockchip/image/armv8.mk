148095cfc39505f8855168f9aa5642058823cc52
leo <huangliquan@xzrobot.com>
2023-09-28 09:36:54 +0800

    暂不适配uboot

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -121,7 +121,7 @@ define Device/i2c_cputools-i6p
   DEVICE_VENDOR := I2C
   DEVICE_MODEL := CPUTOOLS I6P
   SOC := rk3566
-  UBOOT_DEVICE_NAME := cputools-i6p-rk3566
+  UBOOT_DEVICE_NAME := lubancat1-rk3566
   IMAGE/sysupgrade.img.gz := boot-common | boot-script rk356x | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-r8169
 endef
